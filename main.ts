031f6d28c4014526abef8b47aa778c96f0671ca4
Henrique Linhares <124594651+henriquelinhares@users.noreply.github.com>
2023-09-26 18:36:23 +0000

    exercício

---
+++ main.ts
@@ -0,0 +1,13 @@
+let nome: string = 'Herrique'; 
+let numero: number = 30;
+let verdade: boolean = true; 
+let vazio: null
+
+let lista: Array<number> = [1, 2, 3, 4];
+
+let objeto: object = {
+    nome: 'Luiz',
+    idade: 30,
+    estudante: 'Javascript',
+}
+
