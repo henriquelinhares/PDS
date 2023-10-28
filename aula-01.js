44fa761a8835ba8547afbdd1f6c95f3e3e34a51f
Luiz Linhares <linhares.luiz8@gmail.com>
2023-10-28 17:07:35 -0300

    aula

---
+++ aula-01.js
@@ -0,0 +1,5 @@
+//O comando tsc faz a compilação do arquivo em ts para js. 
+var nome = 'Bruno';
+var canal = 'CFB';
+console.log(nome);
+console.log(canal);
---
+++ aula-01.ts
@@ -0,0 +1,8 @@
+//O comando tsc faz a compilação do arquivo em ts para js.
+// Caso ocorra erro ao utilizar o tsc, utilizar o comando: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser; para reparar. 
+
+const nome = 'Bruno';
+const canal = 'CFB';
+
+console.log(nome);
+console.log(canal); 
