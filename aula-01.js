01991863a36c6833a5041754bbe1607308d65ca5
Luiz Linhares <linhares.luiz8@gmail.com>
2023-10-28 20:34:15 -0300

    aula

--- aula-01.js
+++ aula-01.js
@@ -1,7 +1,22 @@
 //O comando tsc faz a compilação do arquivo em ts para js. Sempre utilizar para atualizar o arquivo js.
 // Caso ocorra erro ao utilizar o tsc, utilizar o comando: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser; para reparar. 
+// Para gerar o arquivo em js contentendo as configurações mais atualizadas, o código é: tsc nomedoarquivo.ts --target "ESNEXT" 
+// Para gerar um arquivo de configuração, o comando é: tsc --init
+//Passos para modificar o arquivo gerado: implementar esnext em vez do ano no trecho target. 
 var nome = 'Bruno';
 var canal = 'CFB';
 var curso = 'typescript';
 console.log(nome);
 console.log(canal);
+console.log(curso);
+var teste = /** @class */ (function () {
+    function teste(canal, curso) {
+        this.canal = '';
+        this.curso = '';
+        this.canal = canal;
+        this.curso = curso;
+    }
+    return teste;
+}());
+var c1 = new teste('CFC', 'typescript');
+console.log(c1);
--- aula-01.ts
+++ aula-01.ts
@@ -2,9 +2,28 @@
 
 // Caso ocorra erro ao utilizar o tsc, utilizar o comando: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser; para reparar. 
 
+// Para gerar o arquivo em js contentendo as configurações mais atualizadas, o código é: tsc nomedoarquivo.ts --target "ESNEXT" 
+
+// Para gerar um arquivo de configuração, o comando é: tsc --init
+//Passos para modificar o arquivo gerado: implementar esnext em vez do ano no trecho target. 
+
 const nome = 'Bruno';
 const canal = 'CFB';
 const curso = 'typescript'
 
 console.log(nome);
 console.log(canal); 
+console.log(curso);
+
+
+class teste {
+    canal:string = ''
+    curso:string = ''
+    constructor(canal:string, curso:string){
+        this.canal = canal
+        this.curso = curso
+    }
+}
+
+let c1 = new teste('CFC', 'typescript')
+console.log(c1);
