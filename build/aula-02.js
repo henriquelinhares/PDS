4410c94b3587e7157fefeea8531c681421781951
Luiz Linhares <linhares.luiz8@gmail.com>
2023-10-28 22:16:57 -0300

    aula

--- build/aula-02.js
+++ build/aula-02.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('testando');
+let nome1 = 'Luiz';
+let nome2 = 'Henrique';
+let idade1 = 30;
+console.log(nome2 + ' ' + idade1);
+class pessoa {
+    constructor(nome, idade, profissão) {
+    }
+}
+let p1 = new pessoa('Linhares', 30, 'Biólogo');
+console.log(p1.nome);
--- src/aula-02.ts
+++ src/aula-02.ts
@@ -1,3 +1,20 @@
 // Utilizando o comando tsc -w, ativa o modo  Watch input files, criando e modificando automaticamente os arquivos em JS.
 
-console.log('testando');
+//Tipagem em typescript 
+
+let nome1 = 'Luiz';
+let nome2: string = 'Henrique';
+let idade1: number = 30; 
+
+console.log(nome2 + ' ' + idade1);
+
+class pessoa {
+    constructor(nome:string, idade:number, profissão:string){
+
+    }
+}
+
+let p1 = new pessoa('Linhares', 30, 'Biólogo');
+console.log(p1);
+
+
