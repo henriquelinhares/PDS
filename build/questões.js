9ff40c7b4aa063d088d8c0a26e8ca80b785c741c
Henrique Linhares <124594651+henriquelinhares@users.noreply.github.com>
2023-10-31 19:13:47 +0000

    aula

--- build/questões.js
+++ build/questões.js
@@ -1,11 +1,25 @@
 "use strict";
+class Medico {
+    id;
+    crm;
+    nome;
+    especialidade;
+    constructor(id, crm, nome, especialidade) {
+        this.id = id;
+        this.crm = crm;
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+}
 const medicos = [];
 console.log(medicos);
-function adicionar(nome, crm) {
-    let n = nome;
-    let c = crm;
-    let med = nome + crm;
+function adicionar() {
+    let id = 1;
+    let crm = 12345;
+    let nom = 'Luiz';
+    let esp = 'Qualquer';
+    let med = new Medico(id, crm, nom, esp);
     medicos.push(med);
 }
-console.log(adicionar('Luiz', 12345));
-console.log(medicos);
+adicionar();
+console.log(medicos[0]);
--- src/questões.ts
+++ src/questões.ts
@@ -1,13 +1,28 @@
+class Medico {
+    id
+    crm
+    nome 
+    especialidade
+    constructor(id:number, crm:number, nome:string, especialidade?:string) {
+        this.id = id;
+        this.crm = crm;
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+}
+
 const medicos:Array<any> = []; 
+
 console.log(medicos);
 
-function adicionar(nome: string, crm:number): void {
-    let n = nome;
-    let c = crm;
-    let med = nome + crm;
+function adicionar():any {
+    let id = 1;
+    let crm = 12345;
+    let nom = 'Luiz';
+    let esp = 'Qualquer';
+    let med = new Medico(id, crm, nom, esp)
     medicos.push(med);
 }
-console.log(adicionar('Luiz', 12345));
-
-console.log(medicos);
+adicionar();
+console.log(medicos[0]);
 
