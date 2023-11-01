fe4738698c41a6a2ecdbf6e0467fbfad9eb6522c
Henrique Linhares <linhares.luiz8@gmail.com>
2023-11-01 17:26:38 -0300

    aula

--- build/aula-13-metodos-classes.js
+++ build/aula-13-metodos-classes.js
@@ -10,6 +10,21 @@ class Computadorr {
         this.cpuComp = cpuComp;
         this.ligado = false;
     }
+    info() {
+        console.log(`Nome.: ${this.nomeComp}.`);
+        console.log(`ram.: ${this.ramComp} GB de ram.`);
+        console.log(`CPU.: ${this.cpuComp} núcleos.`);
+        console.log(`O computador está ligado?: ${this.ligado ? "Sim" : "Não"}.`);
+        console.log('----------------------------------');
+    }
+    ligar() {
+        this.ligado = true;
+    }
+    desligar() {
+        this.ligado = false;
+    }
 }
-const compe1 = new Computador('Primeiro Computador', 16, 8);
-console.log(comp1);
+const compe1 = new Computadorr('Primeiro Computador', 16, 8, true);
+compe1.ligado = true;
+compe1.desligar();
+compe1.info();
--- src/aula-13-metodos-classes.ts
+++ src/aula-13-metodos-classes.ts
@@ -1,3 +1,7 @@
+//Metodos das classes. 
+//Os métodos são funções das classes. 
+
+//As vezes é necessário proteger os parametros e os métodos, tornando elas privadas. 
 
 class Computadorr{ 
     nomeComp:string;  
@@ -11,9 +15,29 @@ class Computadorr{
         this.cpuComp = cpuComp;
         this.ligado = false; 
     }  
+
+    info(): void{ //Método info. O metodo está publico. 
+        console.log(`Nome.: ${this.nomeComp}.`);
+        console.log(`ram.: ${this.ramComp} GB de ram.`);
+        console.log(`CPU.: ${this.cpuComp} núcleos.`);
+        console.log(`O computador está ligado?: ${this.ligado?"Sim":"Não"}.`);//Forma de ternar. 
+        console.log('----------------------------------'); 
+    }
+
+    ligar():void{
+        this.ligado = true;
+    }
+    desligar():void {
+        this.ligado = false;
+    }
+
 }
 
+const compe1 = new Computadorr('Primeiro Computador', 16, 8, true);
 
-const compe1 = new Computador('Primeiro Computador', 16, 8); 
+compe1.ligado = true; //Os atributos e metodos da classe estão publicos, permitindo a alteração dos valores.
+compe1.desligar(); //Utilizando o método para desligar. 
 
-console.log(comp1); 
+compe1.info(); 
+
+ 
