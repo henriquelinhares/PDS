83b51760608337ad3261e212cd4df32244453104
Luiz Linhares <linhares.luiz8@gmail.com>
2023-11-02 16:44:44 -0300

    aula

--- build/aula-14-herança-classes.js
+++ build/aula-14-herança-classes.js
@@ -6,12 +6,25 @@ class Conta {
         this.numero = numero;
         this.titular = titular;
     }
+    gerarNumeroConta() {
+        return Math.floor(Math.random() * 100000) + 1;
+    }
 }
 class ContaPessoaFisica extends Conta {
+    cpf;
+    constructor(cpf, numero, titular) {
+        super(numero, titular);
+        this.cpf = cpf;
+    }
 }
 class ContaPessoaJuridica extends Conta {
+    cnpj;
+    constructor(cnpj, numero, titular) {
+        super(numero, titular);
+        this.cnpj = cnpj;
+    }
 }
-const cont1 = new ContaPessoaFisica(12345, 'Henrique');
+const cont1 = new ContaPessoaFisica(123456789, 12345, 'Henrique');
 console.log(cont1);
-const cont2 = new ContaPessoaJuridica(67890, 'Alice');
+const cont2 = new ContaPessoaJuridica(123456789, 67890, 'Alice');
 console.log(cont2);
--- src/aula-14-herança-classes.ts
+++ src/aula-14-herança-classes.ts
@@ -9,17 +9,29 @@ class Conta{
         this.numero = numero;
         this.titular = titular;
     }
+    gerarNumeroConta():number{
+       return Math.floor(Math.random()*100000) + 1 //Sistema para gerar numero aleatório. Caso seja implementado, não será mais necessãrio passar o atributo numero no construtor. Então deve ficar: this.numero = gerarNumeroConta();
+    }
 }
 
 class ContaPessoaFisica extends Conta{//extends é utilizado para herdar atributos e metodos da classe Conta(classe mãe).
+    cpf:number; //Parametro exclusivo desta classe filha.
+    constructor(cpf:number, numero:number, titular:string){//Se precisar adicionar um atributo diferente na classe filha, é preciso criar um construtor com o novo parametro e com os parametros da classe mãe.
+        super(numero,titular)//Utilizado para invocar a classe mãe da classe que estiver trabalhando.
+        this.cpf = cpf;
+    }
 }
-class ContaPessoaJuridica extends Conta{
-
+class ContaPessoaJuridica extends Conta{ //Classe filha. 
+    cnpj:number;
+    constructor(cnpj:number, numero:number, titular: string){
+        super(numero, titular);
+        this.cnpj = cnpj; 
+    }
 }
 
-const cont1 = new ContaPessoaFisica(12345, 'Henrique');
+const cont1 = new ContaPessoaFisica(123456789, 12345, 'Henrique');
 console.log(cont1);
 
-const cont2 = new ContaPessoaJuridica(67890, 'Alice');
+const cont2 = new ContaPessoaJuridica(123456789, 67890, 'Alice');
 console.log(cont2);
 
