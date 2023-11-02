dcaecce5b4d5af56ebba674fe7a71ebea95c2f68
Luiz Linhares <linhares.luiz8@gmail.com>
2023-11-02 15:49:23 -0300

    aula

---
+++ build/Questoes-classes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Aluno = exports.Pessoa = void 0;
+class Pessoa {
+    nome = 'Henrique';
+    idade = 30;
+    email = 'linhares.luiz8@gmail.com';
+}
+exports.Pessoa = Pessoa;
+let P1 = new Pessoa();
+console.log(P1);
+class Aluno {
+}
+exports.Aluno = Aluno;
+var Pessoa = ;
+{
+}
---
+++ src/questoes-classes.ts
@@ -0,0 +1,11 @@
+export class Pessoa{
+    nome:string = 'Henrique';
+    idade:number = 30;
+    email:string = 'linhares.luiz8@gmail.com';
+}
+let P1 = new Pessoa()
+console.log(P1);
+
+export class Aluno import Pessoa{
+
+}
