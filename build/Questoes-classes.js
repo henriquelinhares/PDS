677773eaf6aa5b387f259c38367b7ab071b31e79
Henrique Linhares <linhares.luiz8@gmail.com>
2023-11-06 19:14:39 -0300

    aula

--- build/Questoes-classes.js
+++ build/Questoes-classes.js
@@ -1,17 +1,59 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aluno = exports.Pessoa = void 0;
+exports.Pessoa = void 0;
 class Pessoa {
-    nome = 'Henrique';
-    idade = 30;
-    email = 'linhares.luiz8@gmail.com';
+    nome;
+    idade;
+    email;
+    constructor(nome, idade, email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
 }
 exports.Pessoa = Pessoa;
-let P1 = new Pessoa();
-console.log(P1);
-class Aluno {
+class Aluno extends Pessoa {
+    matricula;
+    constructor(nome, idade, email, matricula) {
+        super(nome, idade, email);
+        this.matricula = matricula;
+    }
 }
-exports.Aluno = Aluno;
-var Pessoa = ;
-{
+class Retangulo {
+    largura;
+    altura;
+    constructor(altura, largura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+    calculoArea(largura, altura) {
+        let calc = largura * altura;
+        return calc;
+    }
+    calculoPerimetro(l1, l2, l3, l4) {
+        let calc = l1 * l2 * l3 * l4;
+        return calc;
+    }
 }
+class ContaBancaria {
+    saldo;
+    numeroConta;
+    constructor(saldo, numeroConta) {
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+    }
+    deposito(n) {
+        if (n > 0 && n < 1000) {
+            this.saldo = n;
+        }
+        else {
+            console.log('Valor não permitido.');
+        }
+    }
+    saque() {
+    }
+}
+let cont10 = new ContaBancaria(0, 123456);
+console.log(cont10.saldo);
+cont10.deposito(10);
+console.log(cont10.saldo);
--- src/questoes-classes.ts
+++ src/questoes-classes.ts
@@ -1,11 +1,67 @@
 export class Pessoa{
-    nome:string = 'Henrique';
-    idade:number = 30;
-    email:string = 'linhares.luiz8@gmail.com';
+    nome:string;
+    idade:number;
+    email:string;
+    constructor(nome:string, idade:number, email:string) {
+        this.nome = nome; 
+        this.idade = idade;
+        this.email = email;
+    }
 }
-let P1 = new Pessoa()
-console.log(P1);
 
-export class Aluno import Pessoa{
-
+class Aluno extends Pessoa{
+    matricula:number; 
+    constructor(nome: string, idade: number, email:string, matricula:number){
+        super(nome, idade, email);
+        this.matricula = matricula;
+    }
 }
+
+class Retangulo{
+    largura:number;
+    altura:number;
+    constructor(altura:number, largura:number){
+        this.largura = largura;
+        this.altura = altura;
+    }
+    
+    public calculoArea(largura:number, altura:number): number{
+        let calc = largura * altura;
+        return calc;
+    }
+
+    public calculoPerimetro(l1:number, l2:number, l3:number, l4:number): number{ //Perimetro com quatro lados.
+        let calc = l1 * l2 * l3 * l4;
+        return calc;
+    }
+}
+// let ret = new Retangulo(10, 35);
+// console.log('A área possui ' + ret.calculoArea(10, 35) + 'm2.');
+
+class ContaBancaria{
+    saldo:number;
+    numeroConta:number;
+    constructor(saldo:number, numeroConta:number){
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+    }
+    deposito(n:number): void{
+        if(n > 0 && n < 1000){
+            this.saldo = n;
+        }else{
+            console.log('Valor não permitido.');
+        }
+    }
+    saque(){
+
+    }
+}
+
+let cont10 = new ContaBancaria(0, 123456);
+console.log(cont10.saldo);
+
+cont10.deposito(10);
+console.log(cont10.saldo);
+
+
+
