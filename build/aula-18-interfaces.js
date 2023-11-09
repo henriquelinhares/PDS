19484f7800eb770dd8854e6186a416c658a6582e
Luiz Linhares <linhares.luiz@gmail.com>
2023-11-08 21:53:02 -0300

    aula

--- build/aula-18-interfaces.js
+++ build/aula-18-interfaces.js
@@ -1,7 +1,39 @@
 "use strict";
 let cursoo;
 cursoo = { tituloo: 'Typescript', maxAlunos: 25, aulas: 25, };
-console.log(cursoo);
+function iniciarCurso() {
+    console.log('Teste!');
+}
+;
 let curso1 = { nomeCurso: 'Typescript', aulasCurso: 35, maxAlunos: 25 };
 let curso2 = { nomeCurso: 'Javascript', aulasCurso: 35, maxAlunos: 25 };
 let curso3 = { nomeCurso: 'Java', aulasCurso: 35, maxAlunos: 25 };
+let curso4 = { nomeCurso: 'PHP', aulasCurso: 35, maxAlunos: 25 };
+console.log(iniciarCurso());
+let cursoEnfermagem = { nomeCurso: 'Enfermagem', aulasCurso: 50, maxAlunos: 40, materias: 10, meses: 12 };
+let cursoFisica = { nomeCurso: 'Fisica', aulasCurso: 50, maxAlunos: 40, materias: 15, meses: 15 };
+console.log(cursoEnfermagem);
+console.log(cursoFisica);
+class Livros {
+    titulo;
+    autor;
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+}
+class livrosBiologia extends Livros {
+    taxaPagar = 0;
+    assunto;
+    capitulos;
+    constructor(titulo, autor, assunto, capitulos) {
+        super(titulo, autor);
+        this.assunto = assunto;
+        this.capitulos = capitulos;
+    }
+    pagar(valor) {
+        return valor + this.taxaPagar;
+    }
+}
+const l1bio = new livrosBiologia('Genetica', 'Henrique', 'Evolução genetica', 15);
+l1bio.pagar(10);
--- src/aula-18-interfaces.ts
+++ src/aula-18-interfaces.ts
@@ -6,7 +6,6 @@ let cursoo:{ //Objeto literal declarado, mas ainda sem valores.
 
 cursoo = {tituloo:'Typescript', maxAlunos: 25, aulas: 25,} //Atibuindo valores ao objeto.
 
-console.log(cursoo);
 
 //A maneira acima, no objeto literal, se quisermos criar um novo objeto para um outro curso, teremos que replicar a forma que foi feito para o cursoo. Se quisermos criar novos objetos a partir de um já existente, como é visto em classes, precisamos utilizar o recurso de interfces. 
 
@@ -15,9 +14,74 @@ console.log(cursoo);
 interface cursos { //Objeto criado utilizando interface. Permite criar outros objetos a partir dele. 
     nomeCurso:string,
     aulasCurso:number,
-    maxAlunos:number,
+    maxAlunos?:number, //A interrogação informa que esse é um parametro opcional. 
+
+    iniciarCurso?(teste:string):void; //Criando uma função com parametro em interface. 
 }
+function iniciarCurso():void{
+    console.log('Teste!');
+};
+
 
 let curso1:cursos = {nomeCurso:'Typescript', aulasCurso:35, maxAlunos: 25}; //Novo objeto criado. 
 let curso2:cursos = {nomeCurso:'Javascript', aulasCurso: 35, maxAlunos: 25}; //Novo objeto criado. 
-let curso3:cursos = {nomeCurso:'Java', aulasCurso: 35, maxAlunos: 25}; 
+let curso3:cursos = {nomeCurso:'Java', aulasCurso: 35, maxAlunos: 25};
+let curso4:cursos = {nomeCurso:'PHP', aulasCurso: 35, maxAlunos: 25};  
+
+console.log(iniciarCurso());
+
+
+//Herança em interfaces. 
+
+//Tamém conseguimos uteilzar herança em interfaces de forma semelhante como é em classes. Exemplo. 
+
+interface cursosSaude extends cursos{ //Criamos uma nova interface que vai se basear a partir da interface cursos. 
+    materias:number,                  //Nas interfaces "filhas" os parametros herdados não precisam ser repetidos, eles já serão solicitados
+    meses:number,                     //de forma automatica na criação do objeto. Ex: linha 48. 
+}
+
+interface cursosExatas extends cursos{
+    materias:number,
+    meses:number,
+}
+
+let cursoEnfermagem:cursosSaude = {nomeCurso:'Enfermagem', aulasCurso:50, maxAlunos:40, materias:10, meses:12}; //
+let cursoFisica:cursosExatas = {nomeCurso:'Fisica', aulasCurso:50, maxAlunos:40, materias:15, meses:15};
+
+console.log(cursoEnfermagem);
+console.log(cursoFisica);
+
+
+//Também podemos implementar interfaces em classes. Por exemplo: 
+
+abstract class Livros{
+    titulo:string;
+    autor: string;
+    constructor(titulo:string, autor:string){
+        this.titulo = titulo; 
+        this.autor = autor;
+    }
+}
+
+interface pagarLivro{
+    taxaPagar:number;
+    pagar(valor:number):number; 
+}
+
+class livrosBiologia extends Livros implements pagarLivro{ //Implements é utilizado para importar interfaces.
+    taxaPagar:number = 0; 
+    assunto:string;
+    capitulos:number;
+    constructor(titulo:string, autor:string, assunto:string, capitulos:number){
+        super(titulo,autor);
+        this.assunto = assunto;
+        this.capitulos = capitulos;
+    }
+    pagar(valor:number):number {
+        return valor + this.taxaPagar
+    }
+}
+
+const l1bio = new livrosBiologia('Genetica', 'Henrique', 'Evolução genetica', 15); 
+
+l1bio.pagar(10); 
